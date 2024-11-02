dc47c2afaf2293888c261803b5b8c4983cf94d38
Abraão Santos <115851871+TechAbraao@users.noreply.github.com>
2024-11-02 14:07:50 -0300

    novos componentes

--- AgroSynTech-Frontend/src/App.jsx
+++ AgroSynTech-Frontend/src/App.jsx
@@ -4,12 +4,16 @@ import Navbar from "./components/navbar/index.jsx";
 import "./App.css";
 import Main from "./pages/main/index.jsx";
 import CardsMissao from "./pages/cardsMissao/index.jsx";
+import SobreErnstGotsch from "./pages/sobreErnstGotsch/index.jsx";
+import FooterPage from "./pages/footer/index.jsx";
 
 function App() {
    return (
       <>
          <Main />
          <CardsMissao />
+         <SobreErnstGotsch />
+         <FooterPage />
       </>
    );
 }
---
+++ AgroSynTech-Frontend/src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Footer = () => {
+    return (<footer>
+       <div className="footer-superior">
+
+       </div>
+       <div className="footer-inferior">
+
+       </div>
+    </footer>)
+}
+
+export default Footer;
